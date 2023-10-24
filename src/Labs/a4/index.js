cfc52d991b7e30c76843463bc0741881d4a29196
Isaac Levine <isaacmlevine4@gmail.com>
2023-10-24 16:41:05 -0400

    stuff

--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -4,13 +4,16 @@ import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
 
 function Assignment4() {
+  function sayHello() {
+    alert("Hello");
+  }
   return (
     <div>
       <h1>Assignment 4</h1>
       <Add a={1} b={2} />
       <ClickEvent />
       <PassingDataOnEvent />
-      <PassingFunctions />
+      <PassingFunctions theFunction={sayHello} />
     </div>
   );
 }
