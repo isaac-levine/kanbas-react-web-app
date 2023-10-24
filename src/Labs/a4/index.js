a8e57c88354806f3a0c68769022b86f2ff94f4c6
Isaac Levine <isaacmlevine4@gmail.com>
2023-10-24 16:39:04 -0400

    started lab 4

--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -1,7 +1,16 @@
+import Add from "./Add";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+
 function Assignment4() {
   return (
     <div>
-      <h1>Assignment4</h1>
+      <h1>Assignment 4</h1>
+      <Add a={1} b={2} />
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions />
     </div>
   );
 }
