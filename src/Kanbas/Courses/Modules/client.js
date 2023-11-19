c95818b82ab30b60191eb077c1f633af7c96e43c
Isaac Levine <isaacmlevine4@gmail.com>
2023-11-19 12:58:09 -0500

    one change

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 const COURSES_URL = "http://localhost:4000/api/courses";
-const MODULES_URL = "http://localhost:4000/api/modules";
+// const MODULES_URL = "http://localhost:4000/api/modules";
+// const MODULES_URL =
+//   "https://kanbas-node-server-app-az4u.onrender.com/api/modules";
+const API_BASE = process.env.REACT_APP_API_BASE;
+const MODULES_URL = `${API_BASE}/modules`;
 
 // get all the modules for this course
 export const findModulesForCourse = async (courseId) => {
