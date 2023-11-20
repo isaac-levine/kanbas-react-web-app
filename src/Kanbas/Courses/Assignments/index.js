598beabd6542e6f4cabf13a9d5054e7af3db5c0a
Isaac Levine <isaacmlevine4@gmail.com>
2023-11-19 19:22:52 -0500

    added assignments

--- src/Kanbas/Courses/Assignments/index.js
+++ src/Kanbas/Courses/Assignments/index.js
@@ -1,39 +1,46 @@
-import React from "react";
 import { Link, useParams } from "react-router-dom";
 import db from "../../Database";
-import './index.css'
-
+import "./index.css";
+import React, { useState, useEffect } from "react";
+import { findAssignmentsForCourse } from "./client";
 
 function Assignments() {
-    const { courseId } = useParams();
-    const assignments = db.assignments;
-    // courseAssignments is the list of assignments for this course
-    const courseAssignments = assignments.filter(
-        (assignment) => assignment.course === courseId);
-    return (
-        <div>
-            <div className="card">
-                <div className="card-header">
-                    <button className="btn">
-                        Assignments for course {courseId}
-                    </button>
-                </div>
-                <div className="card-body">
-                    <div id="assignments-list" className="list-group-flush">
-                        {courseAssignments.map((assignment) => (
-                            <Link
-                                key={assignment._id}
-                                to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}
-                                className="list-group-item">
-                                {assignment.title}
-                            </Link>
-                        ))}
-                    </div>
-                </div>
-            </div>
+  // courseAssignments is the list of assignments for this course
+  const courseAssignments = assignments.filter(
+    (assignment) => assignment.course === courseId
+  );
+  const { courseId } = useParams();
+  const [assignments, setAssignments] = useState([]);
 
+  useEffect(() => {
+    const fetchAssignments = async () => {
+      const data = await findAssignmentsForCourse(courseId);
+      setAssignments(data);
+    };
 
+    fetchAssignments();
+  }, [courseId]);
+  return (
+    <div>
+      <div className="card">
+        <div className="card-header">
+          <button className="btn">Assignments for course {courseId}</button>
         </div>
-    );
+        <div className="card-body">
+          <div id="assignments-list" className="list-group-flush">
+            {courseAssignments.map((assignment) => (
+              <Link
+                key={assignment._id}
+                to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}
+                className="list-group-item"
+              >
+                {assignment.title}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Assignments;
