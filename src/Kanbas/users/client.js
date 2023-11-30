f4914ce034e3d31aa81d6951f61da2ab2795a179
Isaac Levine <isaacmlevine4@gmail.com>
2023-11-30 14:00:32 -0500

    changed client base api to hardcoded

--- src/Kanbas/users/client.js
+++ src/Kanbas/users/client.js
@@ -3,7 +3,8 @@ const request = axios.create({
   withCredentials: true,
 });
 
-export const BASE_API = process.env.REACT_APP_BASE_API_URL;
+// export const BASE_API = process.env.REACT_APP_BASE_API_URL;
+export const BASE_API = "https://a6-kanbas-node-server-app-cj4e.onrender.com";
 export const USERS_API = `${BASE_API}/api/users`;
 
 export const signin = async (credentials) => {
